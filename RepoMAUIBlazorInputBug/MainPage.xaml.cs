63c9d38026dadab2c5b9f00dee52a676dc446799
Jennal <jennalcn@gmail.com>
2023-04-15 08:34:43 +0800

    init

---
+++ RepoMAUIBlazorInputBug/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.WebView;
+
+namespace RepoMAUIBlazorInputBug;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        blazorWebView.UrlLoading -= BlazorWebView_UrlLoading;
+        blazorWebView.UrlLoading += BlazorWebView_UrlLoading;
+    }
+
+    private void BlazorWebView_UrlLoading(object sender, UrlLoadingEventArgs e)
+    {
+        e.UrlLoadingStrategy = UrlLoadingStrategy.OpenInWebView;
+    }
+}
---
+++ RepoMAUIBlazorInputBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RepoMAUIBlazorInputBug;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
